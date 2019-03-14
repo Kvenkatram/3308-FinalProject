135d904f8f4523c23b80559b69b03ca8d93a5e9b
Kvenkatram <42844996+Kvenkatram@users.noreply.github.com>
2019-03-14 14:37:04 -0600

    Add files via upload

---
+++ SpotifyWetherTest/js/index.js
@@ -0,0 +1,32 @@
+const KEY = "acedfaaa83a8b8931ecb3d2e2d7f672b";
+
+let searchBtn = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city_name");
+let temperature = document.getElementById("temp");
+
+searchBtn.addEventListener("click", getWeather);
+
+function getWeather(){
+	if (searchInput.value.length != 0 ){
+		let requestLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+KEY;
+		openWeatherCaller(requestLink, responseHandler);
+	}
+}
+
+function openWeatherCaller(url, callback){
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = () =>{
+		 if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+	}
+	httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.send();
+}
+
+function responseHandler(response){
+	let jObj = JSON.parse(response);
+	cityName.innerHTML = jObj.name;
+	temperature.innerHTML = jObj.main.temp;
+}
+

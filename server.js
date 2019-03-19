ecd2fc97de2ca6c84d94633f70fd5cad28f53949
Kvenkatram <karthik.venkatram1@gmail.com>
2019-03-19 16:40:58 -0600

    updated served to included register

--- server.js
+++ server.js
@@ -6,10 +6,35 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
 
 
+//to do: set up db locally
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'ourDb',
+	user: 'postgres',
+	password: '123'
+};
+
+
+
 //display_login
 app.get('/', function(req,res){
-	res.render('test');
+	res.render('login');
 });
+
+//display register page
+app.get('/register',function(req,res){
+	res.render('register');
+});
+
+
+//post login info to db
+
+app.post('/register/submit',function(req,res){
+	res.render('login');
+	console.log("Hellooooo");
+});
+
 
 app.listen(3000);
 console.log('server up on port 3000');

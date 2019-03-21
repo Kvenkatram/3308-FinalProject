94409b7c0420773e83e25953adaba7d254f936f8
allo1327 <46908413+allo1327@users.noreply.github.com>
2019-03-21 13:17:23 -0600

    Attempted to add buttons

---
+++ SpotifyWetherTest/js/index.js
@@ -0,0 +1,31 @@
+const KEY = 0f203d100bf401455f758a60d2d0c31d;
+
+let searchBtn = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city_name");
+let temperature = document.getElementById("temp");
+
+searchBtn.addEventListener("click", getWeather);
+
+function getWeather(){
+	if (searchInput.value.length != 0 ){
+		let requestLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+KEY;
+		openWeatherCaller(requestLink, responseHandler);
+	}
+}
+
+function openWeatherCaller(url, callback){
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = () =>{
+		 if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+	}
+	httpRequest.open("GET", url, true); // true for asynchronous
+    httpRequest.send();
+}
+
+function responseHandler(response){
+	let jObj = JSON.parse(response);
+	cityName.innerHTML = jObj.name;
+	temperature.innerHTML = jObj.main.temp;
+}
